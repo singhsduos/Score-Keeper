863da26da424d0f88716f4f80152f56343b700a7
singhsduos <ns9628491678@gmail.com>
2021-09-29 00:53:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const select = document.querySelector('#playto');
+const btn3 = document.querySelector('.btn-3');
+
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 3;
+let isWinning = false;
+
+const p1 = {
+    score: 0,
+    button: document.querySelector('.btn-1'),
+    btnText: document.querySelector('#btn1Text'),
+    display: document.querySelector('#p1DisplayScore')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('.btn-2'),
+    btnText: document.querySelector('#btn2Text'),
+    display: document.querySelector('#p2DisplayScore')
+}
+
+//  function for updating scores
+function score(player, opponent) {
+    if (!isWinning) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isWinning = true;
+            player.display.classList.add("winColor");
+            opponent.display.classList.add("looserColor");
+            p1.button.classList.add("btn1Disabled");
+            p2.button.classList.add("btn2Disabled");
+            player.btnText.classList.remove('btn-text');
+            opponent.btnText.classList.remove('btn-text');
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+// function for restarting the game
+function reset() {
+    p1.score = 0;
+    p2.score = 0;
+    isWinning = false;
+    p1.display.textContent = p1.score;
+    p2.display.textContent = p2.score;
+    p1.display.classList.remove('winColor', 'looserColor');
+    p2.display.classList.remove('winColor', 'looserColor');
+    p1.button.classList.remove("btn1Disabled");
+    p2.button.classList.remove("btn2Disabled");
+    p1.btnText.classList.add('btn-text');
+    p2.btnText.classList.add('btn-text');
+}
+
+// Reset Button
+btn3.addEventListener('click', reset);
+
+//  Player1 Button
+p1.button.addEventListener('click', function () {
+    score(p1, p2);
+});
+
+// Player2 Button
+p2.button.addEventListener('click', function () {
+    score(p2, p1);
+});
+
+// Select Value Change
+select.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
