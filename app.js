7f8723338a1efe4368021834eabf0313f523f83a
singhsduos <ns9628491678@gmail.com>
2021-09-29 11:42:44 +0530

    alert-box-added

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const select = document.querySelector('#playto');
 const btn3 = document.querySelector('.btn-3');
 
-
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 3;
@@ -9,6 +8,7 @@ let isWinning = false;
 
 const p1 = {
     score: 0,
+    participant: "Player1",
     button: document.querySelector('.btn-1'),
     btnText: document.querySelector('#btn1Text'),
     display: document.querySelector('#p1DisplayScore')
@@ -16,15 +16,21 @@ const p1 = {
 
 const p2 = {
     score: 0,
+    participant: "Player2",
     button: document.querySelector('.btn-2'),
     btnText: document.querySelector('#btn2Text'),
     display: document.querySelector('#p2DisplayScore')
 }
 
+
+
+
+
 //  function for updating scores
 function score(player, opponent) {
     if (!isWinning) {
         player.score += 1;
+        player.display.textContent = player.score;
         if (player.score === winningScore) {
             isWinning = true;
             player.display.classList.add("winColor");
@@ -33,8 +39,8 @@ function score(player, opponent) {
             p2.button.classList.add("btn2Disabled");
             player.btnText.classList.remove('btn-text');
             opponent.btnText.classList.remove('btn-text');
+            alert(`Congratulations! ${player.participant} Won👏🎉🎉🎉 \nPress reset to play again.`);
         }
-        player.display.textContent = player.score;
     }
 }
 
